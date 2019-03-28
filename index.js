47826739eccbee5824865ec5f2f5619ddc2e5637
Jason Beedle <jason_beedle@hotmail.com>
2019-03-28 18:16:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var recipes = {
-  ingredient1: = 'avocado'
-  ingredient2: = 'cheese'
-  ingredient3: = 'bread'
+  ingredient1 = 'avocado'
+  ingredient2 = 'cheese'
+  ingredient3 = 'bread'
 }
+
+
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes,)
