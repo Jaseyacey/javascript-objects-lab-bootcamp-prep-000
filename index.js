57a44f4ae03a8430cb703e459904ef85ec8364e3
Jason Beedle <jason_beedle@hotmail.com>
2019-03-28 19:03:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-var recipes = {
-  
-};
+var recipes = {};
 
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value
-
-  return obj
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
 }
+updateObjectWithKeyAndValue({prop: 1}, 'prop2', 2);
 
-function updateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+destructivelyUpdateObjectWithKeyAndValue({prop: 1}, 'prop2', 2);
 
-  return object.assign({}, obj, {[key]: value})
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj[key];
+}
+deleteFromObjectByKey({prop: 1}, 'prop');
+
+destructivelyDeleteFromObjectByKey(object, key){
+  var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+  return obj[key];
 }
